c027c3b5d55669322703e119e9d739a224873561
Katerina <katyasemenova96@gmail.com>
2018-02-02 14:51:53 +0300

    Initial commit

---
+++ src/javase01/t01/logic/Logic.java
@@ -0,0 +1,10 @@
+package javase01.t01.logic;
+
+/**
+ * Created by Екатерина on 02.02.2018.
+ */
+public class Logic {
+    public String method(){
+        return "I am string in Logic.";
+    }
+}
---
+++ src/javase01/t01/main/Main.java
@@ -0,0 +1,13 @@
+package javase01.t01.main;
+
+/**
+ * Created by Екатерина on 02.02.2018.
+ */
+import javase01.t01.logic.Logic;
+
+public class Main {
+    public static void main(String[] args) {
+        Logic logic = new Logic();
+        System.out.println(logic.method());
+    }
+}
