2895be1c2aa479015d48b8f9485c45f0661c7e7c
Katerina <katyasemenova96@gmail.com>
2018-02-07 16:31:05 +0300

    done with extra code for simple cheking

---
+++ src/javase01/t02/Task2.java
@@ -0,0 +1,28 @@
+package javase01.t02;
+
+/**
+ * Created by Екатерина on 07.02.2018.
+ */
+public class Task2 {
+    //Найти наименьший номер элемента последовательности,
+    // для которого выполняется условие M.
+    // Вывести на экран этот номер и все элементы ai  где i = 1, 2, ..., п.
+    public static void main(String[] args) {
+        System.out.println("znacheniya:");
+        for (int i = 1; i < 10; i++)
+            System.out.println("a min = " + 1/(((double)i+1)*((double) i+1)) + " as i = " + i);
+
+
+
+        double a = 1;
+        double e = 0.01;
+        int n = 0;
+        for (int i = 1; !(a < e); i++) {
+            a = 1/(((double)i+1)*((double) i+1));
+            n = i;
+        }
+        System.out.println("result:");
+        System.out.println("a min = " + a + " as i = " + n);
+
+    }
+}
