43d76dd4e74c8000ef46abf28ae163f507dc778d
Катя <katyasemenova96@gmail.com>
2018-03-31 00:43:20 +0300

    Delete Percussion.java

--- src/homework/javase01/music/instruments/Percussion.java
+++
@@ -1,9 +0,0 @@
-package homework.javase01.music.instruments;
-
-/**
- * Created by Екатерина on 14.02.2018.
- */
-public interface Percussion extends Instrument {
-    void getMethodOfSoundProduction();
-    void setMethodOfSoundProduction(String str);
-}
